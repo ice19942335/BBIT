e782646125f10bd0250971cd80030c953b511761
ice19942335 <ice19942335@gmail.com>
2020-04-05 16:46:24 +0100

    Update tenant response correction

--- Domain/BBIT.Domain/Entities/BBIT.WEB.Service/Contracts/V1/Responses/Tenant/Success/SuccessUpdateTenantResponse.cs
+++ Domain/BBIT.Domain/Entities/BBIT.WEB.Service/Contracts/V1/Responses/Tenant/Success/SuccessUpdateTenantResponse.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using BBIT.Domain.Entities.DTO.Flat;
+﻿using BBIT.Domain.Entities.DTO.Tenant;
 
 namespace BBIT.Domain.Entities.BBIT.WEB.Service.Contracts.V1.Responses.Tenant.Success
 {
     public class SuccessUpdateTenantResponse
     {
-        public string Id { get; set; }
+        public bool Status { get; set; }
 
-        public string Name { get; set; }
-
-        public string Surname { get; set; }
-
-        public string PersonalCode { get; set; }
-
-        public string DateOfBirth { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public string Email { get; set; }
-
-        public FlatDto Flat { get; set; }
+        public TenantDto Tenant { get; set; }
     }
 }
--- Servcies/BBIT.WEB.Service/SwaggerExamples/Responses/Tenant/Success/SuccessUpdateTenantResponseExample.cs
+++ Servcies/BBIT.WEB.Service/SwaggerExamples/Responses/Tenant/Success/SuccessUpdateTenantResponseExample.cs
@@ -2,6 +2,7 @@
 using BBIT.Domain.Entities.BBIT.WEB.Service.Contracts.V1.Responses.Tenant.Success;
 using BBIT.Domain.Entities.DTO.Flat;
 using BBIT.Domain.Entities.DTO.House;
+using BBIT.Domain.Entities.DTO.Tenant;
 using Swashbuckle.AspNetCore.Filters;
 
 namespace BBIT.WEB.Service.SwaggerExamples.Responses.Tenant.Success
@@ -12,30 +13,34 @@ namespace BBIT.WEB.Service.SwaggerExamples.Responses.Tenant.Success
         {
             return new SuccessUpdateTenantResponse
             {
-                Id = "4644e41b-c19e-4f24-96f3-013103030c5a",
-                Name = "Name",
-                Surname = "Surname",
-                PersonalCode = "12345",
-                DateOfBirth = DateTime.Today.ToString("d"),
-                PhoneNumber = "+37112345678",
-                Email = "email@mail.com",
-                Flat = new FlatDto
+                Status = true,
+                Tenant = new TenantDto
                 {
-                    Id = "ce71e57d-c18c-47ce-94a5-2363cb187a5a",
-                    FlatNumber = "1",
-                    Level = 15,
-                    AmountOfRooms = 5,
-                    AmountOfTenants = 0,
-                    TotalArea = 300,
-                    HouseRoom = 400,
-                    House = new HouseDto
+                    Id = "4644e41b-c19e-4f24-96f3-013103030c5a",
+                    Name = "Name",
+                    Surname = "Surname",
+                    PersonalCode = "12345",
+                    DateOfBirth = DateTime.Today.ToString("d"),
+                    PhoneNumber = "+37112345678",
+                    Email = "email@mail.com",
+                    Flat = new FlatDto
                     {
-                        Id = "77adbe2d-6f73-48ac-bf74-29167fbb89db",
-                        HouseNumber = "1",
-                        StreetName = "High street",
-                        City = "London",
-                        Country = "United Kingdom",
-                        PostCode = "E1 7AD"
+                        Id = "ce71e57d-c18c-47ce-94a5-2363cb187a5a",
+                        FlatNumber = "1",
+                        Level = 15,
+                        AmountOfRooms = 5,
+                        AmountOfTenants = 0,
+                        TotalArea = 300,
+                        HouseRoom = 400,
+                        House = new HouseDto
+                        {
+                            Id = "77adbe2d-6f73-48ac-bf74-29167fbb89db",
+                            HouseNumber = "1",
+                            StreetName = "High street",
+                            City = "London",
+                            Country = "United Kingdom",
+                            PostCode = "E1 7AD"
+                        }
                     }
                 }
             };
